d5b18d30e0d46cf7e296faac604e0ffea0a81807
Cheng Hu <williamcheng.hu@gmail.com>
2019-03-28 19:45:28 +0200

    Updates

---
+++ precache-manifest.d8fc946ee4783a1aeadb77819ce1f1a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d012e544a999bd9c69a9adc27af7450d",
+    "url": "/yousician_new_test/static/media/fingerprint-white.d012e544.png"
+  },
+  {
+    "revision": "48af3378640006e45dea",
+    "url": "/yousician_new_test/static/js/runtime~main.48af3378.js"
+  },
+  {
+    "revision": "62776258610f62acc08d",
+    "url": "/yousician_new_test/static/js/main.62776258.chunk.js"
+  },
+  {
+    "revision": "1fba66fdce1dbab98879",
+    "url": "/yousician_new_test/static/js/2.1fba66fd.chunk.js"
+  },
+  {
+    "revision": "737e2866b437c5dcb00dfd95c057a2e8",
+    "url": "/yousician_new_test/index.html"
+  }
+];
